599912af14359dde2d5af6a4447a7cf9813c57bf
Pradeep987654321 <105903974+Pradeep987654321@users.noreply.github.com>
2022-06-18 09:45:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
-app=Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "Hello"
+ 
+app = Flask(__name__)
+CORS(app) 
+        
+@app.route('/movie', methods=['GET'])
+def recommend_movies():
+    res = recommendation.results(request.args.get('title'))
+    return jsonify(res)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(port = 5000, debug = True)
